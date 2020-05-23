5d01ff2933d429a8af2e4acbbdf00cdae9ad0a15
Vinsea <smallheidou@qq.com>
2020-05-23 14:34:58 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports={
+    pages: {
+        index: {
+            entry: 'docs/src/main.js',
+            template: 'docs/public/index.html',
+            filename: 'index.html',
+            title: 'UI Toolkit for Vue2.0.',
+        },
+    }
+}
